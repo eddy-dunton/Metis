2947d337f7f9af982aac2e4d734627be8468d024
edward-dunton <edward_dunton@btconnect.com>
2021-02-03 20:22:03 +0000

    Working db PoC

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 
 console.log("Connecting to db");
 const db = sqlite3.cached.Database("pallas.db");
@@ -8,16 +8,17 @@ console.log("Connected to db!")
 const app = express();
 
 app.get('/', async (req, res) => {
-  const result = await db.get("SELECT * FROM Test;");
-  console.log(result);
-  res.send(result);
+  db.get("SELECT * FROM Test WHERE test = 1;", (err, row) => {
+    console.log(row);
+    res.send(row);
+  });
 });
 
 app.get('/test', (req, res) => {
-  console.log("Req");
-  async () => {
-    res.send("toast");
-  }
+  db.get("SELECT * FROM Test WHERE test = 2;", (err, row) => {
+    console.log(row);
+    res.send(row);
+  });
 });
 
 app.listen(3000, () => console.log("Listening"));
